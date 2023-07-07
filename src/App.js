56f8a1a3b21a1958b0dea0a82ccfba79eec70a15
Jean-Paul <Jean.paul.bassil@outlook.com>
2023-07-07 16:25:58 +0300

    added real time firebase database

--- src/App.js
+++ src/App.js
@@ -3,12 +3,25 @@ import "./App.css";
 import Auth from "./components/Auth";
 import Layout from "./components/Layout";
 import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 function App() {
+  const cart = useSelector((state) => state.card);
+
   const isAuth = useSelector((state) => state.auth.isAuthenticated);
-  const cardItems = useSelector((state) => state.card.items);
-  console.log(cardItems);
-  console.log(isAuth);
+  useEffect(() => {
+    const sendRequest = async () => {
+      const res = await fetch(
+        "https://redux-http-668ef-default-rtdb.firebaseio.com/cartItems.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+      const data = await res.json();
+    };
+    sendRequest();
+  }, [cart]);
   return (
     <div className="App">
       {isAuth && <Layout />}
